dc1fbff86f03faf412c61134c8d22b5f220724c7
Taani Maama <taani1maama@live.com>
2021-01-06 14:49:10 -0800

    Created the orm script so I can call back these functions for the promises.

--- db/orm.js
+++ db/orm.js
@@ -1,1 +1,21 @@
-const connection = require('./connection')
+const connection = require('./connection');
+
+module.exports = {
+
+    selectAll() {
+        return connection.query('SELECT * FROM burgers')
+    },
+
+    insertOne(burger) {
+        return connection.query('INSERT INTO burgers VALUES (?)', [{
+            name: burger.name
+        }])
+    },
+
+    updateOne(burger) {
+        return connection.query('UPDATE burgers SET ? WHERE ?', [
+            {name: burger.name},
+            {devoured: burger.devoured}
+        ])
+    }
+}
