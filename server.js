381438b1a553ec7b29fd0afb073e097d2500a3fe
Taani Maama <taani1maama@live.com>
2021-01-08 18:19:44 -0800

    Trying to get heroku to deploy.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-const router = require('./controllers/burgersController')
+const routes = require('./controllers/burgersController')
 
 app.use(express.static('public'));
 
@@ -15,7 +15,7 @@ app.use(express.json());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use(router)
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log("App listening on http://LocalHost:" + PORT);
