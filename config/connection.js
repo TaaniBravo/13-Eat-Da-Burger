ede32ad73b98cdfe4696617f4b7b838d7fa81a8c
Taani Maama <taani1maama@live.com>
2021-01-08 20:37:08 -0800

    Trying to fix issues with heroku.

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const util = require('util');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     user: 'root',
     password: '2b.D_?rtwbjm',
