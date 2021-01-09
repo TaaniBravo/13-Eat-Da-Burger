61915fc4789e24c23ee4fceb0ffbce271c9b4e16
Taani Maama <taani1maama@live.com>
2021-01-09 14:23:53 -0800

    Updated the connection.js to use JawsDB.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const util = require('util');
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+let connection;
+
+if ( process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: 'localhost',
     port: 3306,
-    user: '	eu606onx24x5mgk9',
-    password: 'r7ff0lvc94s4zpzl',
-    database: 'wawy8pf78jfl2ws4'
+    user: 'root',
+    password: '2b.D_?rtwbjm',
+    database: 'burgers_db'
 });
+};
 
 connection.connect(err => {
     if (err) {
